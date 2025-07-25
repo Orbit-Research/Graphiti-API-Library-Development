44e8c6906dd2b421cc02fd6561732eb29c139582
danieloconnellard1 <daniel.oconnell@ard1.net>
2025-07-25 19:00:48 -0400

    Testing the C wrapper

--- binding/C/main.c
+++ binding/C/main.c
@@ -1,6 +1,11 @@
+#include <windows.h>
 #include <stdio.h>
 #include "capi.h"
 
 int main() {
-    
+    HMODULE hDll = LoadLibrary("libGraphiti_C.dll");
+    if (hDll == NULL) {
+        printf("Could not load DLL.\n");
+        return 1;
+    }
 }
